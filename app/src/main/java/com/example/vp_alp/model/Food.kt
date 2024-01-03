20eb25c7523f73433ccf4aeb4412f08e66e36e3b
AsukaaNao <christiantanjung27@gmail.com>
2024-01-03 11:35:20 +0700

    p don

--- app/src/main/java/com/example/vp_alp/model/Food.kt
+++ app/src/main/java/com/example/vp_alp/model/Food.kt
@@ -4,8 +4,9 @@ data class Food(
     val id:Int,
     val restaurant: Restaurant,
     val category: Category,
+    val name: String,
     val description: String,
-    val price: Int,
-    val rating:Float,
+    val price: Double,
+    val rating:Double,
     val image: String
 )
